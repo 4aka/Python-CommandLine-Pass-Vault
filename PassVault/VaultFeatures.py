a2a0b1c971ebd323e433ddd29c233c4180fa9b0b
4aka <7nuclear@gmail.com>
2025-04-16 16:09:01 +0300

    Add fixes

--- PassVault/VaultFeatures.py
+++ PassVault/VaultFeatures.py
@@ -1,6 +1,7 @@
 import sqlite3
-from Variables import vault_file, create_entry_sql, search_sql, show_favorites_sql
+from Variables import vault_file, show_favorites_sql, create_entry_sql, search_sql
 from Vault import decrypt, copy_to_clipboard, get_bool, encrypt
+from GeneratePassword import generate_password
 
 
 def search():
@@ -9,26 +10,27 @@ def search():
     result = None
     conn = sqlite3.connect(vault_file)
     cursor = conn.cursor()
-    cursor.execute(search_sql, site)
+    cursor.execute(search_sql, ('%' + site + '%',))
     entry = cursor.fetchone()
     conn.close()
     if entry:
         result = decrypt(entry[0])
-
-    print("Password copied to clipboard")
     copy_to_clipboard(result)
+    print("Password copied to clipboard")
 
 
 def create_entry():
     site = input('URL: ')
-    username = input('Username: ')
-    password = input('Password: ').encode()
+    username = input('Username: ').encode()
+    password = generate_password(20)
+    print('Generated password: ' + password)
     is_favorite = get_bool('Favorite? y/n: ')
 
     conn = sqlite3.connect(vault_file)
     cursor = conn.cursor()
-    encrypted_password = encrypt(password)
-    cursor.execute(create_entry_sql, (site, username, encrypted_password, is_favorite))
+    encrypted_password = encrypt(password.encode())
+    encrypted_username = encrypt(username)
+    cursor.execute(create_entry_sql, (site, encrypted_username, encrypted_password, is_favorite))
     conn.commit()
     conn.close()
 
@@ -37,11 +39,18 @@ def show_favorite():
     conn = sqlite3.connect(vault_file)
     cursor = conn.cursor()
     cursor.execute(show_favorites_sql)
+    entry = cursor.fetchall()
+    print(entry)
     conn.commit()
     conn.close()
-
+    print()
     search()
 
 
 def edit_entry():
-    return None
+    site = input('Input url to edit entry: ')
+    conn = sqlite3.connect(vault_file)
+    cursor = conn.cursor()
+    cursor.execute(search_sql, ('%' + site + '%',))
+
+
