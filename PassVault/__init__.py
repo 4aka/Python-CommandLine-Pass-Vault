b238d622757d7768fb9b9b5a44376cfa2095caef
4aka <7nuclear@gmail.com>
2025-04-16 16:07:41 +0300

    Update user menu

--- PassVault/__init__.py
+++ PassVault/__init__.py
@@ -1,37 +1,38 @@
 from VaultFeatures import search, create_entry, show_favorite, edit_entry
 from Vault import login
 import sys
+from GeneratePassword import generate_password
 
 
 def show_wellcome():
     print('''Wellcome to your VAULT!''')
+    action = None
 
+    while action != "z":
 
-def show_menu():
-    action = input('''
-    s - search
-    c - create
-    f - favorites
-    e - edit
-    z - exit
-    ''')
-    match action:
-        case "s":
-            search()
-        case "c":
-            create_entry()
-        case "f":
-            show_favorite()
-        case "e":
-            edit_entry()
-        case "z":
+        action = input('''
+        s - search
+        c - create
+        f - favorites
+        e - edit
+        z - exit
+        ''')
+        match action:
+            case "s":
+                search()
+            case "c":
+                create_entry()
+            case "f":
+                show_favorite()
+            case "e":
+                edit_entry()
+        if action == "z":
             sys.exit()
 
 
 def main():
-    show_wellcome()
     login()
-    show_menu()
+    show_wellcome()
 
 
 # Run main
