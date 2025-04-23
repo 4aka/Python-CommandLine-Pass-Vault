42f4e746974246b1277ba9bc7e211eb70b431dda
4aka <7nuclear@gmail.com>
2025-04-23 17:15:11 +0300

    Exclude hardcode

--- PassVault/HashData.py
+++ PassVault/HashData.py
@@ -1,8 +1,9 @@
 import hashlib
+from Variables import hash_count
 
 
 def hash_data(data):
     hashed_data = data
-    for x in range(50):
+    for x in range(hash_count):
         hashed_data = hashlib.sha3_256(hashed_data.encode('ascii')).hexdigest()
     return hashed_data
