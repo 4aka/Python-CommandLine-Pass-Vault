a39e6fa631e4c328d7400e180ec1e75e6b9da2ee
4aka <7nuclear@gmail.com>
2025-04-23 17:15:31 +0300

    Clean up code

--- PassVault/PasswordActions.py
+++ PassVault/PasswordActions.py
@@ -1,9 +1,6 @@
 import base64
-
-from cryptography.fernet import Fernet
-
 from HashData import hash_data
-from Variables import passowrd_file, passowrd_db_file
+from Variables import passowrd_file
 
 
 def create_password(password):
@@ -13,29 +10,9 @@ def create_password(password):
     f.close()
 
 
-def create_db_password():
-    f = open(passowrd_db_file, "wb")
-    key = Fernet.generate_key()
-    base_key = base64.b64encode(key)
-    f.write(base_key)
-    f.close()
-
-
-def read_db_password():
-    f = open(passowrd_db_file, "rb")
-    bytes_file = f.read()
-    password = base64.b64decode(bytes_file)
-    f.close()
-    return password
-
-
 def compare_password_with_existed(password):
     existed_password = read_password()
     return existed_password == password
-
-
-def compare_passwords(password_1, password_2):
-    return password_1 == password_2
 
 
 def read_password():
@@ -52,6 +29,6 @@ def read_password_bytes():
     return password
 
 
-def input_password(input_text):
+def input_data(input_text):
     return input(input_text)
 
