dd29db5598617e3d1884dd6a8e3b0f8e5d8c058d
4aka <7nuclear@gmail.com>
2025-04-25 23:12:27 +0300

    Rename func, replace functions

--- PassVault/Assertions.py
+++ PassVault/Assertions.py
@@ -1,5 +1,5 @@
 import sqlite3
-from Variables import passowrd_file, vault_file_path, vault_file, search_site
+from Variables import passowrd_file, vault_file_path, vault_file, search_site, select_all
 
 
 def is_password_file_exists():
@@ -18,28 +18,53 @@ def is_vault_file_exists():
         print('There is no any created vaults')
 
 
-def get_unique_site_name():
+def is_site_name_unique():
     conn = sqlite3.connect(vault_file)
     cursor = conn.cursor()
 
     site = input('site: ')
+    while site == '':
+        site = input('Site should be a string! Try again: ')
+        if site != '':
+            break
+
     cursor.execute(search_site, (site,))
-    conn.commit()
     entry = cursor.fetchone()
 
     # Define if site exists
-    # TODO replace to assertions
     if entry is not None:
         while site is not None:
             site = input('Site has already existed! Create another name: ')
             cursor.execute(search_site, (site,))
-            conn.commit()
             entry = cursor.fetchone()
             if entry is None:
                 break
-
+    conn.close()
     return site
+
+
+def get_user_name():
+    username = input('Username: ')
+    while username == '':
+        username = input('Username should be a string! Try again: ')
+        if username != '':
+            break
+    return username
 
 
 def passwords_equels(password_1, password_2):
     return password_1 == password_2
+
+
+def is_vault_empty():
+    conn = sqlite3.connect(vault_file)
+    cursor = conn.cursor()
+    cursor.execute(select_all)
+    entry = cursor.fetchone()
+    conn.close()
+    if entry is None:
+        print('Vault is empty! Create at list one entry')
+        return True
+    else:
+        return False
+
